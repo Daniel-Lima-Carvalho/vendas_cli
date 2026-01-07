a83a1947deaf2b9d32a58a3b823aac42585924d6
Daniel Lima <T-Daniel.Lima@dufry.com>
2026-01-07 02:39:33 -0300

    Adjusting encoding

--- modules/csv_utils.py
+++ modules/csv_utils.py
@@ -4,7 +4,7 @@ import logging
 
 def read_csv_file(file_path: str) -> List[Dict[str, str]]:
     logging.info(f"Reading csv file at path: {file_path}...")
-    with open(file_path) as sale_file:
+    with open(file_path, encoding='utf-8') as sale_file:
         reader = csv.DictReader(sale_file)
         data = [line for line in reader]
     logging.info(f"File read!")
